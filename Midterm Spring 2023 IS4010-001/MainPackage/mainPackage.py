f928730baa629c3fcc1091b6a6973698e9ba008c
nicomp42 <nicholdw@ucmail.uc.edu>
2023-03-04 18:04:12 -0500

    Second Commit

--- Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
+++ Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
@@ -6,13 +6,12 @@ totalPlays = 1000
 won = 0
 lost = 0
 for i in range(0, totalPlays):
-    try:
-        if play():
-            won = won + 1
-        else:
-            lost = lost + 1
-    except:
-        pass
+
+    if play():
+        won = won + 1
+    else:
+        lost = lost + 1
+
 print("Total plays:", totalPlays, 
       ", won = ", won, 
       ", lost = ", lost, 
