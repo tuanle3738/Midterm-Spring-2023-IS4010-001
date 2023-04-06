ae22a2a92714839fd785e95686fb39a2269ffea0
le2te <le2te@LCBLAB2230-12.uccob.uc.edu>
2023-04-06 00:15:29 -0400

    Add main.py

--- Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
+++ Midterm Spring 2023 IS4010-001/MainPackage/mainPackage.py
@@ -1,14 +1,29 @@
 # main.py
 
-from videoGamePackage.videoGame import *
+# Name: Tuan Le
+# email: le2te@mail.uc.edu    
+# Assignment Title: Assignment 10
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: This module sends an API request then prints out some information
+# Citations: Code and guide from Professor Nicholson
+# Anything else that's relevant: My API key is gEOeqq1LwIMU9Gpqe1Fd1G7bdS9JvgPLYLDcMbL5
 
-totalPlays = 1000
-won = 0
-lost = 0
-for i in range(0, totalPlays):
+import json
+import requests
 
-    if play():
-        won = won + 1
-    else:
-        lost = lost + 1
+# Send the data request
+response = requests.get('https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size&api_key=gEOeqq1LwIMU9Gpqe1Fd1G7bdS9JvgPLYLDcMbL5')
 
+# Parse the results into a Python dictionary
+json_string = response.content
+parsed_json = json.loads(json_string) # Now we have a python dictionary
+
+#print(parsed_json)
+#print(parsed_json['data'][0]['description'])
+#print(parsed_json['data'][0]['directionsInfo'])
+#total = int(parsed_json['total']) # The number of parks that were returned
+
+# print out how many students each school has in 2013
+for school in parsed_json['results']:
+    print (school['school.name'], 'has', school['2013.student.size'], 'students in 2013')
--- Midterm Spring 2023 IS4010-001/videoGamePackage/videoGame.py
+++
@@ -1,9 +0,0 @@
-# videoGame.py
-import random
-def play():
-    '''
-    returns True if you won, False if you lost 
-    '''
-    if random.randint(0,100) == 10:
-        raise Exception("Game Crashed")
-    return random.choice([True, False])
